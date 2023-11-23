c605569cd4f062d302da0ad71e46806a402b05d0
abdielmg007 <abdiel.mugo@gmail.com>
2023-11-23 02:04:01 -0600

    Initial Commit

---
+++ golden-marc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  golden-marc
+//
+//  Created by Abdiel Mg on 23/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Golden-Marc")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ golden-marc/golden_marcApp.swift
@@ -0,0 +1,17 @@
+//
+//  golden_marcApp.swift
+//  golden-marc
+//
+//  Created by Abdiel Mg on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct golden_marcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
